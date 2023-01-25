918356d1c86e5fad78ce0735138705f092fe588b
Shitalgadakh44@gmail.com <shitalgadakh44@gmail.com>
2023-01-26 01:57:41 +0530

    Third commit

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Mymodel(models.Model):
+
+    field1 = models.CharField(max_length=250)
+    field2 = models.CharField(max_length=250)
