e569ffb8c1e5067843285fceb58230e6f899d099
Shitalgadakh44@gmail.com <shitalgadakh44@gmail.com>
2023-01-26 03:06:07 +0530

    Third commit in app1

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app1.models import MyModels
+from app1.models import Mymodel
 # Register your models here.
 
-admin.site.register(MyModel)
+admin.site.register(Mymodel)
